9a5a8870a1ce1ec9cbcf0cdd300edf316c3ca30e
Aung <bigblackhacker2015@gmail.com>
2021-10-14 22:35:09 +0630

    added logic

--- android/app/src/main/kotlin/com/example/masalah/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/masalah/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.masalah
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
+    
 }
