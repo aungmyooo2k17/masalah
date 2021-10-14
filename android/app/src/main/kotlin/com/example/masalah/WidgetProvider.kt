a7ce653a373e8d961624e0628a8b8f666d35cdc1
Aung <bigblackhacker2015@gmail.com>
2021-10-15 01:22:39 +0630

    widget added

--- android/app/src/main/kotlin/com/example/masalah/WidgetProvider.kt
+++ android/app/src/main/kotlin/com/example/masalah/WidgetProvider.kt
@@ -7,6 +7,12 @@ import android.widget.RemoteViews
 import es.antonborri.home_widget.HomeWidgetBackgroundIntent
 import es.antonborri.home_widget.HomeWidgetLaunchIntent
 import es.antonborri.home_widget.HomeWidgetProvider
+import java.util.TimerTask
+
+import java.util.Timer
+
+
+
 
 class AppWidgetProvider : HomeWidgetProvider() {
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray, widgetData: SharedPreferences) {
@@ -18,20 +24,31 @@ class AppWidgetProvider : HomeWidgetProvider() {
                         MainActivity::class.java)
                 setOnClickPendingIntent(R.id.widget_root, pendingIntent)
 
-                val counter = widgetData.getInt("_counter", 0)
+                val counter = widgetData.getString("_counter", "0")
 
-                var counterText = "Your counter value is: $counter"
+                var counterText = "Your: $counter"
 
-                if (counter == 0) {
+                if (counter == "0") {
                     counterText = "You have not pressed the counter button"
                 }
+
+                 //
 
                 setTextViewText(R.id.tv_counter, counterText)
 
                 // Pending intent to update counter on button click
                 val backgroundIntent = HomeWidgetBackgroundIntent.getBroadcast(context,
                         Uri.parse("myAppWidget://updatecounter"))
-                setOnClickPendingIntent(R.id.bt_update, backgroundIntent)
+
+                setOnClickPendingIntent(R.id.llRefresh, backgroundIntent)
+
+                val timer = Timer()
+                timer.schedule(object : TimerTask() {
+                    override fun run() {
+                        //what you want to do
+
+                    }
+                }, 0, 15000)
             }
             appWidgetManager.updateAppWidget(widgetId, views)
         }
