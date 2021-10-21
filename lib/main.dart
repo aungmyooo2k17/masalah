bcff0b3eab906560ec9f42fa88a5b5b7cb5d2dc9
soethan98 <soethan762@gmail.com>
2021-10-21 17:11:52 +0630

    Clean up prayer_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'dart:async';
 
-import 'package:adhan/adhan.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:home_widget/home_widget.dart';
 import 'package:masalah/common/constants/color_constant.dart';
 import 'package:masalah/screens/converter_screen.dart';
 import 'package:masalah/screens/masalah_category_screen.dart';
@@ -11,78 +9,28 @@ import 'package:masalah/screens/prayer_time_screen.dart';
 import 'package:masalah/screens/qibla_screen.dart';
 import 'package:connectivity/connectivity.dart';
 import 'package:get/get.dart';
-import 'package:masalah/util/date_time_util.dart';
-import 'package:masalah/util/local_notification.dart';
 import 'package:masalah/util/locale_string.dart';
-import 'package:workmanager/workmanager.dart';
 
-// void callbackDispatcher() {
-//   Workmanager().executeTask((taskName, inputData) async {
-//     //show the notification
-//     LocalNotification.Initializer();
-//     LocalNotification.ShowOneTimeNotification(DateTime.now());
-
-//     return Future.value(true);
-//   });
-// }
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // HomeWidget.registerBackgroundCallback(backgroundCallback);
-  // await Workmanager().initialize(callbackDispatcher);
-  // await Workmanager().registerPeriodicTask("test_workertask", "test_workertask",
-  //     inputData: {"data1": "value1", "data2": "value2"},
-  //     frequency: Duration(minutes: 1),
-  //     initialDelay: Duration(minutes: 1));
 
   runApp(MyApp());
 }
 
-// dynamic backgroundCallback(Uri? uri) async {
-//   DateTime? selectedDate = DateTime.now();
-//   PrayerTimes? prayerTimes;
-//   PrayerTimes? prayerTimeNow;
-//   Coordinates? myCoordinates;
-//   CalculationParameters? params;
-//   myCoordinates =
-//       Coordinates(16.8409, 96.1735); // Replace with your own location lat, lng.
-//   params = CalculationMethod.karachi.getParameters();
-//   params.madhab = Madhab.hanafi;
-//   prayerTimes = PrayerTimes.today(myCoordinates, params);
-//   prayerTimeNow = PrayerTimes.today(myCoordinates, params);
-//   print("*******************updateingggggggggggggggg");
-//   if (uri!.host == 'updatecounter') {
-//     print("*******************################");
-//     String _counter = "0";
-//     await HomeWidget.getWidgetData<String>('_counter', defaultValue: "0")
-//         .then((value) {
-//       _counter = value!;
-//     });
-//     await HomeWidget.saveWidgetData<String>('_counter',
-//         DateTimeUtil().prayerTimeFormat(prayerTimeNow.fajr).toString());
-//     await HomeWidget.updateWidget(
-//         name: 'AppWidgetProvider', iOSName: 'AppWidgetProvider');
-//   }
-// }
 
 /// This is the main application widget.
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static const String _title = 'Flutter Code Sample';
 
   @override
   Widget build(BuildContext context) {
     return MyStatefulWidget();
-    // return const MaterialApp(
-    //   title: _title,
-    //   debugShowCheckedModeBanner: false,
-    //   home: MyStatefulWidget(),
-    // );
+   
   }
 }
 
-/// This is the stateful widget that the main application instantiates.
 class MyStatefulWidget extends StatefulWidget {
   const MyStatefulWidget({Key? key}) : super(key: key);
 
@@ -90,11 +38,10 @@ class MyStatefulWidget extends StatefulWidget {
   State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
 }
 
-/// This is the private State class that goes with MyStatefulWidget.
+
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+ 
   static List<Widget> _widgetOptions = <Widget>[
     MasalahCategoryScreen(),
     PrayerTimeScreen(),
---
+++ lib/prayer_time/mapper/prayer_time_ui_mapper.dart
@@ -0,0 +1,50 @@
+import 'package:masalah/common/constants/color_constant.dart';
+import 'package:masalah/model/prayer_item/prayer_time_entity.dart';
+import 'package:masalah/model/ui_prayer_time.dart';
+import 'package:masalah/util/date_time_util.dart';
+
+class PrayerTimeUiMapper {
+  List<UiPrayerTimeItem> mapUiPrayerItems(
+          List<PrayerTimeEntity> prayerTimeList) =>
+      prayerTimeList.map((e) => mapUiPrayerItem(e)).toList();
+
+  UiPrayerTimeItem mapUiPrayerItem(PrayerTimeEntity prayerTimeEntity) {
+    return UiPrayerTimeItem(
+        name: prayerTimeEntity.prayerName,
+        currentPrayerTime:
+            DateTimeUtil().prayerTimeFormat(prayerTimeEntity.prayerTime),
+        isMuteForCurrentPrayer: false);
+  }
+
+  UiPrayerTimeItemCard mapUiPrayerCardItem(
+      PrayerTimeEntity currentPrayer, PrayerTimeEntity nextPrayerTime) {
+    return UiPrayerTimeItemCard(
+        currentPrayerTimeItem: mapUiPrayerItem(currentPrayer),
+        nextPrayerName: nextPrayerTime.prayerName,
+        nextPrayerTime:
+            DateTimeUtil().prayerTimeHumanReadable(nextPrayerTime.prayerTime),
+        currentPrayerImage: _getPrayerImage(currentPrayer));
+  }
+
+  String _getPrayerImage(PrayerTimeEntity currentPrayer) {
+    if (currentPrayer.prayerName == "Fajr") {
+      return AppImages.fajr;
+    }
+    if (currentPrayer.prayerName == "Sunrise") {
+      return AppImages.surise;
+    }
+    if (currentPrayer.prayerName == "Dhuhr") {
+      return AppImages.duhr;
+    }
+    if (currentPrayer.prayerName == "Asr") {
+      return AppImages.asir;
+    }
+    if (currentPrayer.prayerName == "Maghrib") {
+      return AppImages.magrib;
+    }
+    if (currentPrayer.prayerName == "Isha") {
+      return AppImages.isha;
+    }
+    return AppImages.magrib;
+  }
+}
