b83e2e4b004363b961df7ad1077fb9b5ea73c295
soethan98 <soethan762@gmail.com>
2022-04-13 00:33:24 +0630

    fix:Localization working by separating main screen from main.dart (#6)
    Co-authored-by: Soe Than <soethan@appvantage.asia>

---
+++ lib/screens/main/main_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:masalah/common/extensions/string_extensions.dart';
+
+import '../../common/constants/color_constant.dart';
+import '../../common/constants/translation_constants.dart';
+import '../converter_screen.dart';
+import '../masalah_category_screen.dart';
+import '../prayer_time/prayer_time_screen.dart';
+import '../qibla_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  static List<Widget> _widgetOptions = <Widget>[
+    MasalahCategoryScreen(),
+    PrayerTimeScreen(),
+    QiblaScreen(),
+    ConverterScreen()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('assets/images/masalah.png')),
+            label: TranslationConstants.gold.t(context) ?? "",
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('assets/images/adhan.png')),
+            label: 'Prayer Time',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('assets/images/qibla.png')),
+            label: 'Qibla',
+          ),
+          BottomNavigationBarItem(
+            icon: ImageIcon(AssetImage('assets/images/converter.png')),
+            label: 'Calculator',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: AppColors.selectedIconColor,
+        backgroundColor: AppColors.primaryText,
+        unselectedItemColor: AppColors.unSelectedIconColor,
+        onTap: _onItemTapped,
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+    );
+  }
+}
